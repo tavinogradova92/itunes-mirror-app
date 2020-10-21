541b0cb74b753e8e60d30ef3d85632e1b7706755
tavinogradova92 <tavinogradova92@gmail.com>
2020-10-21 13:18:07 +0200

    Creating the first frontend page

---
+++ src/main/java/com/company/tanja/springDemo/controllers/GreetingController.java
@@ -0,0 +1,15 @@
+package com.company.tanja.springDemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model; //You may nee to add this manually
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/main/resources/index.php
@@ -0,0 +1,1 @@
+<?php header( 'Location: /greeting.html' ) ;  ?>
