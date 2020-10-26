9b695e75066495ad6b515358777bf18bc87a80df
tavinogradova92 <tavinogradova92@gmail.com>
2020-10-26 10:35:49 +0100

    The main page layout changed. Cards with lists created

--- src/main/java/com/company/tanja/springDemo/controllers/WebController.java
+++ src/main/java/com/company/tanja/springDemo/controllers/WebController.java
@@ -1,16 +1,10 @@
 package com.company.tanja.springDemo.controllers;
 
-import com.company.tanja.springDemo.data_access.CustomerAPIRequests;
 import com.company.tanja.springDemo.data_access.WebRequests;
-import com.company.tanja.springDemo.models.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
 
 @Controller
 public class WebController {
